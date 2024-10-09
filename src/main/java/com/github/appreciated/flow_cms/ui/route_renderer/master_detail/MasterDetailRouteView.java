550a39a65570784d287c3b9632e5df3c638d70e5
Johannes Goebel <GoebelJohannes@gmx.net>
2024-10-10 00:24:12 +0200

    Nachtrag

--- src/main/java/com/github/appreciated/flow_cms/ui/route_renderer/master_detail/MasterDetailRouteView.java
+++ src/main/java/com/github/appreciated/flow_cms/ui/route_renderer/master_detail/MasterDetailRouteView.java
@@ -4,13 +4,14 @@ import com.github.appreciated.flow_cms.config.model.RouteConfig;
 import com.github.appreciated.flow_cms.service.DynamicEntityManagerService;
 import com.github.appreciated.flow_cms.ui.entity_detail.FlowCmsEntityDetailRendererFactory;
 import com.github.appreciated.flow_cms.ui.entity_item_renderer.card.FlowCmsEntityItemRendererFactory;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class MasterDetailRouteView extends VerticalLayout {
     public MasterDetailRouteView(int i, RouteConfig config, DynamicEntityManagerService entityManagerService, FlowCmsEntityItemRendererFactory entityCardRendererFactory, FlowCmsEntityDetailRendererFactory detailRendererFactory) {
-        HorizontalLayout header = new HorizontalLayout(new H3(getTranslation(config.getTitle())));
+        HorizontalLayout header = new HorizontalLayout(new H2(getTranslation(config.getTitle())));
         header.setPadding(true);
         DefaultMasterDetailRenderer virtualGrid = new DefaultMasterDetailRenderer(i, config, entityManagerService, entityCardRendererFactory, detailRendererFactory);
         add(header, virtualGrid);
