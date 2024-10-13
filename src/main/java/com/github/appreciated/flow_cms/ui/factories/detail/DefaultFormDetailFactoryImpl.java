b3db4a93fc42506916b767e4319db088cfae7ff3
Johannes Goebel <GoebelJohannes@gmx.net>
2024-10-14 01:13:43 +0200

    Back-Button hinzugefügt

--- src/main/java/com/github/appreciated/flow_cms/ui/factories/detail/DefaultFormDetailFactoryImpl.java
+++ src/main/java/com/github/appreciated/flow_cms/ui/factories/detail/DefaultFormDetailFactoryImpl.java
@@ -9,8 +9,10 @@ import com.github.appreciated.flow_cms.service.GenericEntity;
 import com.github.appreciated.flow_cms.ui.components.H2WithHasValue;
 import com.github.appreciated.flow_cms.ui.factories.form.FormCreator;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -18,8 +20,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
 
-import static com.vaadin.flow.component.button.ButtonVariant.LUMO_ERROR;
-import static com.vaadin.flow.component.button.ButtonVariant.LUMO_PRIMARY;
+import static com.vaadin.flow.component.button.ButtonVariant.*;
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
 /**
@@ -74,7 +75,7 @@ public class DefaultFormDetailFactoryImpl implements TurboCrudDetailFactory {
         binder.setBean(entity);
 
         // Generic Save button
-        Button saveButton = new Button(layout.getTranslation("button.save.title"), event -> {
+        Button save = new Button(layout.getTranslation("button.save.title"), event -> {
             try {
                 binder.writeBean(entity);
                 entityManagerService.updateRecordById(table, EntityUtil.getId(entity), entity);
@@ -86,17 +87,27 @@ public class DefaultFormDetailFactoryImpl implements TurboCrudDetailFactory {
                 notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
             }
         });
-        saveButton.addThemeVariants(LUMO_PRIMARY);
+        save.addThemeVariants(LUMO_PRIMARY);
 
         // Generic Delete button
-        Button deleteButton = new Button(layout.getTranslation("button.delete.title"), event -> {
+        Button delete = new Button(layout.getTranslation("button.delete.title"), event -> {
             entityManagerService.deleteRecordById(table, EntityUtil.getId(entity));
             Notification.show(layout.getTranslation("form.notification.successfully-deleted"));
         });
-        deleteButton.addThemeVariants(LUMO_PRIMARY, LUMO_ERROR);
+        delete.addThemeVariants(LUMO_PRIMARY, LUMO_ERROR);
+
+        // Generic Delete button
+        Button back = new Button(VaadinIcon.ANGLE_LEFT.create(), event -> UI.getCurrent().getPage().getHistory().back());
+        back.getStyle().set("font-size", "1.5em");
+        back.addThemeVariants(LUMO_TERTIARY_INLINE);
+
+        HorizontalLayout headerBar = new HorizontalLayout();
+        if (!isWrapped){
+            headerBar.add(back);
+        }
 
         // Add the form and buttons to the layout
-        HorizontalLayout headerBar = new HorizontalLayout(titleComponent, saveButton, deleteButton);
+        headerBar.add(titleComponent, save, delete);
         headerBar.setAlignItems(CENTER);
         if (!hideHeader) {
             layout.add(headerBar);
