91c4fff4db019c64ef5de03a976c2ac1aa750746
Johannes Goebel <GoebelJohannes@gmx.net>
2025-07-14 15:23:24 +0200

    Update `JpaVortexCrudForeignKeyResolutionStrategy` to use `String` in `ReflectionService` replacing `FieldId`, and adjust method call accordingly

--- jooq/src/main/java/com/github/appreciated/vortex_crud/jooq/service/syntactic_sugar/JooqField.java
+++ jooq/src/main/java/com/github/appreciated/vortex_crud/jooq/service/syntactic_sugar/JooqField.java
@@ -8,55 +8,55 @@ import org.jooq.TableRecord;
 
 import java.util.List;
 
-public class JooqField extends Field<Class<? extends TableRecord<?>>, TableField<?,?>> {
+public class JooqField extends Field<TableRecord<?>, TableField<?,?>> {
 
 
     public JooqField(Class<? extends VortexCrudFieldFactory> factory) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory);
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory);
     }
 
     public JooqField(Class<? extends VortexCrudFieldFactory> factory, boolean primary) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory, primary);
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory, primary);
     }
 
     public JooqField(Class<? extends VortexCrudFieldFactory> factory, String values) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory, values);
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory, values);
     }
 
     public JooqField(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory, primary, required);
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory, primary, required);
     }
 
     public JooqField(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required, Validation validation) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory, primary, required, validation);
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory, primary, required, validation);
     }
 
-    public JooqField(Class<? extends VortexCrudFieldFactory> factory, TableField<?,?> field, TableField<?,?> filterField, Class<? extends TableRecord<?>> dataStore, List<TableField<?,?>> children) {
-        super((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>) factory, field, filterField, dataStore, children);
+    public JooqField(Class<? extends VortexCrudFieldFactory> factory, TableField<?,?> field, TableField<?,?> filterField, Class<TableRecord<?>> dataStore, List<TableField<?,?>> children) {
+        super((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>) factory, field, filterField, dataStore, children);
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory, primary));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary) {
+        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>)factory, primary));
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, String values) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory, values));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, String values) {
+        return new Builder<>(new Field<>(factory, values));
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory, primary, required));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required) {
+        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>)factory, primary, required));
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required, Validation validation) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory, primary, required, validation));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, boolean primary, boolean required, Validation validation) {
+        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>)factory, primary, required, validation));
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, TableField<?,?> field, TableField<?,?> filterField, Class<? extends TableRecord<?>> dataStore, List<TableField<?,?>> children) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory, field, filterField, dataStore, children));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory, TableField<?,?> field, TableField<?,?> filterField, Class<TableRecord<?>> dataStore, List<TableField<?,?>> children) {
+        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>)factory, field, filterField, dataStore, children));
     }
 
-    public static Builder<Class<? extends TableRecord<?>>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory) {
-        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<Class<? extends TableRecord<?>>, TableField<?,?>>>)factory));
+    public static Builder<TableRecord<?>, TableField<?,?>> of(Class<? extends VortexCrudFieldFactory> factory) {
+        return new Builder<>(new Field<>((Class<? extends VortexCrudFieldFactory<TableRecord<?>, TableField<?,?>>>)factory));
     }
 }
 
