785d484aa84b4b7b959b3eea09a3d29acc05b4a0
Johannes Goebel <GoebelJohannes@gmx.net>
2025-07-24 18:37:41 +0200

    Remove redundant comments in `Application.java` files for JOOQ and JPA examples

--- examples/jpa-sqlite-example/src/main/java/com/github/appreciated/vortex_crud/example/jpa/Application.java
+++ examples/jpa-sqlite-example/src/main/java/com/github/appreciated/vortex_crud/example/jpa/Application.java
@@ -7,18 +7,9 @@ import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The entry point of the Spring Boot application.
- * <p>
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- */
 @SpringBootApplication
 @Theme(variant = Lumo.DARK)
 @Push
-//
-// Make sure the working directory is set to the module path
-//
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
