3d5dfad960cc4af93685f8d07538e34806c78dfc
Johannes Goebel <GoebelJohannes@gmx.net>
2025-07-25 11:29:12 +0200

    Add `ProjectGridTest` and update `ProjectCardsTest` for improved navigation and assertions, extend `VortexCrudBaseUiTestSuite` to include new test.

--- ui-test/src/main/java/com/github/appreciated/vortex_crud/uitest/VortexCrudBaseUiTestSuite.java
+++ ui-test/src/main/java/com/github/appreciated/vortex_crud/uitest/VortexCrudBaseUiTestSuite.java
@@ -1,14 +1,14 @@
 package com.github.appreciated.vortex_crud.uitest;
 
 import com.github.appreciated.vortex_crud.uitest.pages.ProjectCardsTest;
-import org.junit.jupiter.api.Disabled;
+import com.github.appreciated.vortex_crud.uitest.pages.ProjectGridTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.context.annotation.Profile;
 
 @Suite
 @Profile("ui-tests")
-@SelectClasses({ProjectCardsTest.class})
+@SelectClasses({ProjectCardsTest.class, ProjectGridTest.class})
 public class VortexCrudBaseUiTestSuite {
 }
 
--- ui-test/src/main/java/com/github/appreciated/vortex_crud/uitest/pages/ProjectGridTest.java
+++ ui-test/src/main/java/com/github/appreciated/vortex_crud/uitest/pages/ProjectGridTest.java
@@ -8,20 +8,20 @@ import org.openqa.selenium.WebElement;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ProjectCardsTest extends BaseUITest {
+public class ProjectGridTest extends BaseUITest {
 
     @Test
     void checkIfListingVisible() {
-        navigateTo("projects-cards");
+        navigateTo("projects-list");
         WebElement webElement = waitForElement(By.xpath("//*[contains(text(), 'Project Alpha')]"));
-        assertEquals("h4", webElement.getTagName());
+        assertEquals(webElement.getTagName(), "vaadin-grid-cell-content");
     }
 
     @Test
     void checkIfNavigationPossible() {
-        navigateTo("projects-cards");
+        navigateTo("projects-list");
         waitForElement(By.xpath("//*[contains(text(), 'Project Alpha')]")).click();
-        waitForUrlToBe("projects-cards/1");
+        waitForUrlToBe("projects-list/1");
         WebElement inputField = waitForElement(By.tagName("vaadin-text-field"));
         String value = inputField.getAttribute("value");
         Assertions.assertTrue(value.startsWith("Project Alpha"));
