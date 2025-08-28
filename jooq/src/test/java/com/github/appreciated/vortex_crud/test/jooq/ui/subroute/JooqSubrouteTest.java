30c824e348a8c441df417429a5af4e0645db2769
Johannes Goebel <GoebelJohannes@gmx.net>
2025-08-28 15:45:57 +0200

    Fixed @Sql paths

--- jooq/src/test/java/com/github/appreciated/vortex_crud/test/jooq/ui/subroute/JooqSubrouteTest.java
+++ jooq/src/test/java/com/github/appreciated/vortex_crud/test/jooq/ui/subroute/JooqSubrouteTest.java
@@ -3,6 +3,6 @@ package com.github.appreciated.vortex_crud.test.jooq.ui.subroute;
 import com.github.appreciated.vortex_crud.ui_test_base.tests.AbstractSubrouteTest;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql(scripts = "classpath:com/github/appreciated/vortex_crud/test/jooq/ui/subroute/subroute_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "subroute_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class JooqSubrouteTest extends AbstractSubrouteTest {
 }
--- jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/subroute/JpaSubrouteTest.java
+++ jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/subroute/JpaSubrouteTest.java
@@ -3,7 +3,7 @@ package com.github.appreciated.vortex_crud.test.jpa.ui.subroute;
 import com.github.appreciated.vortex_crud.ui_test_base.tests.AbstractSubrouteTest;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql(scripts = "classpath:com/github/appreciated/vortex_crud/test/jpa/ui/subroute/subroute_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "subroute_test.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class JpaSubrouteTest extends AbstractSubrouteTest {
 
 }
