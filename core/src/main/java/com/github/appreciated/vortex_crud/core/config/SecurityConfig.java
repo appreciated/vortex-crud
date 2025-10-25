6b66fc929c7dc3a609dd03eb1f13c3c51f9d628e
Johannes Goebel <GoebelJohannes@gmx.net>
2025-10-26 00:31:56 +0200

    Add `SecurityConfig` and configure custom login view for Spring Security

---
+++ core/src/main/java/com/github/appreciated/vortex_crud/core/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.github.appreciated.vortex_crud.core.config;
+
+import com.github.appreciated.vortex_crud.core.ui.factories.login.LoginView;
+import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
+import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.with(VaadinSecurityConfigurer.vaadin(), configurer -> configurer.loginView(LoginView.class)).build();
+    }
+}
--- core/src/main/java/com/github/appreciated/vortex_crud/core/ui/factories/login/LoginView.java
+++ core/src/main/java/com/github/appreciated/vortex_crud/core/ui/factories/login/LoginView.java
@@ -7,9 +7,7 @@ import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.Route;
 
-@Route("login")
 public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
     private final LoginForm login = new LoginForm();
