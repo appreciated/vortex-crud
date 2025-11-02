d3542d98bebe5e9746b46ead8ce7b973748657b5
Johannes Goebel <GoebelJohannes@gmx.net>
2025-11-02 23:06:56 +0100

    Refactor `JpaGridItemRendererConfiguration` to define factory within builder method and remove redundant factory assignment in `ExampleJpaConfiguration`.

--- jpa/src/main/java/com/github/appreciated/vortex_crud/jpa/service/syntactic_sugar/JpaGridItemRendererConfiguration.java
+++ jpa/src/main/java/com/github/appreciated/vortex_crud/jpa/service/syntactic_sugar/JpaGridItemRendererConfiguration.java
@@ -1,10 +1,13 @@
 package com.github.appreciated.vortex_crud.jpa.service.syntactic_sugar;
 
 import com.github.appreciated.vortex_crud.core.config.model.GridItemRendererConfiguration;
+import com.github.appreciated.vortex_crud.core.ui.factories.item.CardFactory;
+import com.github.appreciated.vortex_crud.core.ui.factories.item.VortexCrudItemFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class JpaGridItemRendererConfiguration {
+    @SuppressWarnings("unchecked")
     public static GridItemRendererConfiguration.GridItemRendererConfigurationBuilder<JpaRepository<?, ?>, String, JpaRepository<?, ?>> builder() {
-        return GridItemRendererConfiguration.builder();
+        return GridItemRendererConfiguration.<JpaRepository<?, ?>, String, JpaRepository<?, ?>>builder().factory((Class<? extends VortexCrudItemFactory<String>>)  (Class<?>) CardFactory.class);
     }
 }
