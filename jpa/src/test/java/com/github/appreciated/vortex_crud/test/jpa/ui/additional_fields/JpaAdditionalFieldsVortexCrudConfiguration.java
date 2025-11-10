2318213aa20ad2d46709f74215a7f1c760956060
Johannes Goebel <GoebelJohannes@gmx.net>
2025-11-10 20:55:31 +0100

    Refactor method naming: replace `name` with `applicationName` in test configuration for consistency

--- jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/additional_fields/JpaAdditionalFieldsVortexCrudConfiguration.java
+++ jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/additional_fields/JpaAdditionalFieldsVortexCrudConfiguration.java
@@ -55,7 +55,7 @@ public class JpaAdditionalFieldsVortexCrudConfiguration implements VortexCrudCon
                 .build());
 
         return JpaApplication.builder()
-                .name("application.name")
+                .applicationName("application.name")
                 .i18nBundlePrefix("ui_test_i18n")
                 .routes(routes)
                 .build();
