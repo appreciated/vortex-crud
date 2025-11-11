4af9ab203fadbba2aa5e70d8d9a6326400997cdc
Johannes Goebel <GoebelJohannes@gmx.net>
2025-11-12 00:05:04 +0100

    Fixed multi-form implementation

--- core/src/main/java/com/github/appreciated/vortex_crud/core/ui/factories/route/form/MultiFormRouteFactory.java
+++ core/src/main/java/com/github/appreciated/vortex_crud/core/ui/factories/route/form/MultiFormRouteFactory.java
@@ -1,23 +1,44 @@
 package com.github.appreciated.vortex_crud.core.ui.factories.route.form;
 
 import com.github.appreciated.vortex_crud.core.config.VortexCrudPathToRouteResolver;
+import com.github.appreciated.vortex_crud.core.config.model.DataStoreConfig;
 import com.github.appreciated.vortex_crud.core.config.model.MultiFormRendererConfiguration;
-import com.github.appreciated.vortex_crud.core.config.model.RouteRenderer;
+import com.github.appreciated.vortex_crud.core.config.model.MultiFormRoute;
 import com.github.appreciated.vortex_crud.core.config.model.RouteRendererConfiguration;
+import com.github.appreciated.vortex_crud.core.entity.data_store.VortexCrudDataStore;
 import com.github.appreciated.vortex_crud.core.entity.data_store.VortexCrudDataStoreFactoryRegistry;
+import com.github.appreciated.vortex_crud.core.entity.reflection.ReflectionService;
 import com.github.appreciated.vortex_crud.core.security.VortexCrudRbacPermissionChecker;
 import com.github.appreciated.vortex_crud.core.service.VortexCrudConfigService;
+import com.github.appreciated.vortex_crud.core.ui.components.H2WithHasValue;
+import com.github.appreciated.vortex_crud.core.ui.components.RouteHeaderBarWithSaveDeleteBack;
 import com.github.appreciated.vortex_crud.core.ui.factories.form.FormCreator;
 import com.github.appreciated.vortex_crud.core.ui.factories.route.DetailRouteSetting;
 import com.github.appreciated.vortex_crud.core.ui.factories.route.VortexCrudRouteFactory;
 import com.github.appreciated.vortex_crud.core.ui.factories.route.VortexCrudRouteFactoryRegistry;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
 import jakarta.annotation.Nullable;
 
 public class MultiFormRouteFactory<ModelClass, FieldType, RepositoryType> implements VortexCrudRouteFactory<ModelClass, FieldType, RepositoryType> {
 
     private final FormRouteFactory<ModelClass, FieldType, RepositoryType> formRouteFactory;
+
+    private final VortexCrudDataStoreFactoryRegistry<ModelClass, FieldType, RepositoryType> dataStoreFactoryRegistry;
+    private final VortexCrudConfigService<ModelClass, FieldType, RepositoryType> configService;
+    private final FormCreator<ModelClass, FieldType, RepositoryType> formCreator;
+    private final VortexCrudRouteFactoryRegistry<ModelClass, FieldType, RepositoryType> factoryRegistry;
+    private final ReflectionService<FieldType> reflectionService;
+    private final VortexCrudRbacPermissionChecker<ModelClass, FieldType, RepositoryType> permissionChecker;
 
     private String titleColumn;
 
@@ -29,23 +50,119 @@ public class MultiFormRouteFactory<ModelClass, FieldType, RepositoryType> implem
             com.github.appreciated.vortex_crud.core.entity.reflection.ReflectionService<FieldType> reflectionService,
             VortexCrudRbacPermissionChecker<ModelClass, FieldType, RepositoryType> permissionChecker
     ) {
+        this.dataStoreFactoryRegistry = dataStoreFactoryRegistry;
+        this.configService = configService;
+        this.formCreator = formCreator;
+        this.factoryRegistry = factoryRegistry;
+        this.reflectionService = reflectionService;
+        this.permissionChecker = permissionChecker;
         this.formRouteFactory = new FormRouteFactory<>(dataStoreFactoryRegistry, configService, formCreator, factoryRegistry, reflectionService, permissionChecker);
     }
 
     @Override
-    public Component renderRoute(Integer currentPathIndex,
-                                 VortexCrudPathToRouteResolver<ModelClass, FieldType, RepositoryType> routeResolver,
-                                 @Nullable DetailRouteSetting detailRouteSetting) {
-        RouteRenderer<ModelClass, FieldType, RepositoryType> routeRenderer = routeResolver.getRouteForIndex(currentPathIndex);
+    public Component renderRoute(
+            Integer currentPathIndex,
+            VortexCrudPathToRouteResolver<ModelClass, FieldType, RepositoryType> routeResolver,
+            @Nullable DetailRouteSetting detailRouteSetting
+    ) {
+        assert routeResolver.getRouteForIndex(currentPathIndex) instanceof MultiFormRoute<ModelClass,FieldType,RepositoryType>;
+        MultiFormRoute<ModelClass, FieldType, RepositoryType> routeProvider = (MultiFormRoute<ModelClass, FieldType, RepositoryType>) routeResolver.getRouteForIndex(currentPathIndex);
+        assert detailRouteSetting != null;
+        return getForm(routeResolver, detailRouteSetting.isWrapped(), detailRouteSetting.isHeaderHidden(), detailRouteSetting.isCreationMode(), false, routeProvider);
+    }
+
+    public VerticalLayout getForm(VortexCrudPathToRouteResolver<ModelClass, FieldType, RepositoryType> routeResolver,
+                                  boolean isWrapped,
+                                  boolean isHeaderHidden,
+                                  boolean creationMode,
+                                  boolean isDeleteButtonHidden,
+                                  MultiFormRoute<ModelClass, FieldType, RepositoryType> routeRenderer) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setPadding(false);
+
+        String prefix = !isWrapped ? layout.getTranslation(routeRenderer.title()) + " / " : "";
+
+        H2WithHasValue titleComponent = new H2WithHasValue();
+        Binder<Object> binder = new Binder<>(Object.class);
+        if (!creationMode) {
+            binder.bindReadOnly(
+                    titleComponent,
+                    entity1 -> prefix + reflectionService.getString(entity1, routeRenderer.configuration().titleField())
+            );
+        } else {
+            titleComponent.setText(titleComponent.getTranslation("button.create.title"));
+        }
+
+        RepositoryType table = routeRenderer.dataStoreKey();
+        DataStoreConfig<ModelClass, FieldType, RepositoryType> tables = configService.configuration().dataStores().get(table);
+        VortexCrudDataStore<FieldType, ModelClass> dataStore = dataStoreFactoryRegistry.getDataStore(table);
 
         MultiFormRendererConfiguration<ModelClass, FieldType, RepositoryType> formConfiguration =
                 (MultiFormRendererConfiguration<ModelClass, FieldType, RepositoryType>) routeRenderer.configuration();
-        Div div = new Div();
-        for (RouteRendererConfiguration<ModelClass, FieldType, RepositoryType> child : formConfiguration.forms()) {
-            assert detailRouteSetting != null;
-            div.add(formRouteFactory.getForm(routeResolver, true, true, detailRouteSetting.isCreationMode(), false, routeRenderer, child));
+
+
+        ModelClass entity;
+        if (creationMode) {
+            entity = dataStore.newInstance();
+        } else {
+            // Traditional mode: fetch by ID from URL path
+            String lastSegment = routeResolver.getLastSegment();
+            entity = dataStore.getRecordById(lastSegment);
         }
-        return div;
+
+        Div Forms = new Div();
+        for (RouteRendererConfiguration<ModelClass, FieldType, RepositoryType> child : formConfiguration.forms()) {
+            Forms.add(formRouteFactory.getForm(routeResolver, true, true, false, false, routeRenderer, child));
+        }
+
+        binder.setBean(entity);
+
+        // Generic Save button
+        ComponentEventListener<ClickEvent<Button>> onSave = event -> {
+            try {
+                binder.writeBean(entity);
+                if (!creationMode) {
+                    dataStore.updateRecord(entity);
+                    binder.setBean(entity);
+                } else {
+                    Object o = dataStore.insertRecord(entity);
+                    binder.setBean(dataStore.getRecordById(o));
+                }
+                Notification notification = Notification.show(layout.getTranslation("form.notification.successfully-saved"));
+                notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+                UI.getCurrent().getPage().getHistory().back();
+            } catch (ValidationException e) {
+                Notification notification = Notification.show(layout.getTranslation("form.notification.failed-to-save", e.getMessage()));
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+            }
+        };
+
+        ComponentEventListener<ClickEvent<Button>> onDelete = event -> {
+            dataStore.deleteRecord(entity);
+            Notification.show(layout.getTranslation("form.notification.successfully-deleted"));
+            UI.getCurrent().getPage().getHistory().back();
+        };
+
+        ComponentEventListener<ClickEvent<Button>> onBack = event -> UI.getCurrent().getPage().getHistory().back();
+
+        // Check write permissions for save/delete buttons
+        boolean hasWriteAccess = permissionChecker == null || permissionChecker.hasUserWriteAccessToRoute(routeRenderer);
+
+        RouteHeaderBarWithSaveDeleteBack headerBar = new RouteHeaderBarWithSaveDeleteBack(
+                isWrapped,
+                creationMode && hasWriteAccess,
+                hasWriteAccess ? onSave : null,
+                null,
+                hasWriteAccess && !isDeleteButtonHidden ? onDelete : null,
+                onBack,
+                titleComponent
+        );
+        if (!isHeaderHidden) {
+            layout.add(headerBar);
+        }
+        layout.add(Forms);
+        layout.setPadding(true);
+        return layout;
     }
 
     @Override
