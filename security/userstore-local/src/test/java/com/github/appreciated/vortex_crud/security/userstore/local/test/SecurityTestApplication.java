91e2f7bc3bde92c64c090c56b88d80301248ab50
Johannes Goebel <GoebelJohannes@gmx.net>
2025-12-08 22:43:38 +0100

    Downgrade Vaadin version from 25.0.0-beta9 to 25.0.0-beta8 across all modules and update related documentation.

--- security/userstore-local/src/test/java/com/github/appreciated/vortex_crud/security/userstore/local/test/SecurityTestApplication.java
+++ security/userstore-local/src/test/java/com/github/appreciated/vortex_crud/security/userstore/local/test/SecurityTestApplication.java
@@ -1,18 +1,12 @@
 package com.github.appreciated.vortex_crud.security.userstore.local.test;
 
-import com.github.appreciated.vortex_crud.security.core.config.LocalStorageVortexCrudRbacPermissionChecker;
-import com.github.appreciated.vortex_crud.security.core.service.LocalStorageUserContextService;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Push
-@ComponentScan(basePackages = "com.github.appreciated.vortex_crud")
-@Import({LocalStorageVortexCrudRbacPermissionChecker.class, LocalStorageUserContextService.class})
 public class SecurityTestApplication implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(SecurityTestApplication.class, args);
