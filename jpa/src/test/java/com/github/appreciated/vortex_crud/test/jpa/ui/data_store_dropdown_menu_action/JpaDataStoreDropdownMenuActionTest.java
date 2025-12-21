28c5d4e9f7bdfb22fbeeb97167c415f720668029
Johannes Goebel <GoebelJohannes@gmx.net>
2025-12-22 00:55:09 +0100

    Remove `DataStoreDropdown`, `DataStoreDropdownConfig`, and related classes, tests, and configurations.

--- jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/data_store_dropdown_menu_action/JpaDataStoreDropdownMenuActionTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.appreciated.vortex_crud.test.jpa.ui.data_store_dropdown_menu_action;
-
-import com.github.appreciated.vortex_crud.ui_test_base.tests.AbstractDataStoreDropdownMenuActionTest;
-import org.springframework.test.context.jdbc.Sql;
-
-@Sql(scripts = "data_store_dropdown_menu_action_test.sql")
-public class JpaDataStoreDropdownMenuActionTest extends AbstractDataStoreDropdownMenuActionTest {
-}
--- jpa/src/test/java/com/github/appreciated/vortex_crud/test/jpa/ui/data_store_dropdown_menu_action/JpaDropdownRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.appreciated.vortex_crud.test.jpa.ui.data_store_dropdown_menu_action;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.github.appreciated.vortex_crud.test.jpa.ui.data_store_dropdown_menu_action.JpaDropdownEntity;
-
-public interface JpaDropdownRepository extends JpaRepository<JpaDropdownEntity, Long> {
-}
