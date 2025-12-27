67ec6f93b4d9ef2f88268a3dc1509252025388a3
Johannes Goebel <GoebelJohannes@gmx.net>
2025-12-27 17:06:46 +0100

    Clear master-detail test data before inserting seed rows in `master_detail_test.sql`.

--- jpa/src/test/resources/com/github/appreciated/vortex_crud/test/jpa/ui/master_detail/master_detail_test.sql
+++ jpa/src/test/resources/com/github/appreciated/vortex_crud/test/jpa/ui/master_detail/master_detail_test.sql
@@ -1,5 +1,7 @@
 -- Seed data for JPA master-detail tests
 
+DELETE FROM master_detail_tasks;
+
 INSERT INTO master_detail_tasks (id, title, description, status) VALUES
   (1, 'Done Task A', 'Finished task', 'Done'),
   (2, 'Done Task B', 'Another finished task', 'Done');
