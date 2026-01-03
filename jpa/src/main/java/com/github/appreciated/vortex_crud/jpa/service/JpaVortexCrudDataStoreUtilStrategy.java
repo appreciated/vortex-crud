30a319d085b8ea77e6f535902689a6daff5822d3
Johannes Goebel <GoebelJohannes@gmx.net>
2026-01-03 19:19:34 +0100

    Refactor `JpaVortexCrudDataStoreUtilStrategy` to support `@Id` annotation lookup and improve ID field detection logic

--- jpa/src/main/java/com/github/appreciated/vortex_crud/jpa/service/JpaVortexCrudDataStoreUtilStrategy.java
+++ jpa/src/main/java/com/github/appreciated/vortex_crud/jpa/service/JpaVortexCrudDataStoreUtilStrategy.java
@@ -1,6 +1,7 @@
 package com.github.appreciated.vortex_crud.jpa.service;
 
 import com.github.appreciated.vortex_crud.core.entity.VortexCrudDataStoreUtilStrategy;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
@@ -13,13 +14,36 @@ public class JpaVortexCrudDataStoreUtilStrategy implements VortexCrudDataStoreUt
             return null;
         }
         try {
-            Field field = record.getClass().getField("id");
+            Field field = findIdField(record.getClass());
+            if (field == null) {
+                throw new IllegalStateException("No @Id field found in class " + record.getClass().getName());
+            }
             field.setAccessible(true);
             Object id = field.get(record);
             return id == null ? null : id.toString();
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Find the ID field in the given entity class by looking for the @Id annotation.
+     * Walks up the class hierarchy to support inheritance.
+     *
+     * @param entityClass The entity class to search in.
+     * @return The ID field, or null if not found.
+     */
+    private Field findIdField(Class<?> entityClass) {
+        Class<?> currentClass = entityClass;
+        while (currentClass != null && currentClass != Object.class) {
+            for (Field field : currentClass.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Id.class)) {
+                    return field;
+                }
+            }
+            currentClass = currentClass.getSuperclass();
+        }
+        return null;
     }
 
     public boolean isNew(Object entity) {
